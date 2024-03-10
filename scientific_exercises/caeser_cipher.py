2d2ee035dd02e33e6bdba0eb0f4a561a6ea21b8c
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-03-10 03:28:08 +0100

    update: added space check

--- scientific_exercises/caeser_cipher.py
+++ scientific_exercises/caeser_cipher.py
@@ -1,4 +1,4 @@
-word = "benjaminnkemtochukwu"
+word = "benjamin nkem"
 alphabets = "abcdefghijklmnopqrstuvwxyz"
 cipher_move = 3
 
@@ -6,8 +6,11 @@ cipher_move = 3
 def encrypt(word_input):
     crypt_word = ""
 
-    for i in word_input:
-        letter = i.lower()
+    for i in word_input.lower():
+        letter = i
+        if letter == " ":
+            crypt_word += " "
+            continue
         letter_index = alphabets.find(letter)
 
         if letter_index + cipher_move > len(alphabets) - 1:
@@ -22,8 +25,11 @@ def encrypt(word_input):
 def decrypt(encrypted_word):
     decrypted_word = ""
 
-    for i in encrypted_word:
-        letter = i.lower()
+    for i in encrypted_word.lower():
+        letter = i
+        if letter == " ":
+            decrypted_word += " "
+            continue
         letter_index = alphabets.find(letter)
 
         if letter_index - cipher_move < 0:
@@ -36,7 +42,7 @@ def decrypt(encrypted_word):
 
 
 encrypt_result = encrypt(word)
-print(encrypt_result)  # ehqmdplqqnhpwrfkxnzx
+print('encrypt_result', encrypt_result)  # ehqmdplqqnhpwrfkxnzx
 
 decrypt_result = decrypt(encrypt_result)
 print(decrypt_result)  # benjaminnkemtochukwu
